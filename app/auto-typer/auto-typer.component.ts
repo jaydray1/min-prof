bff1c55a67047a2d2ebb44463a9889ebef04b47d
JM <jdreher15@gmail.com>
2019-03-26 18:18:04 -0600

    background

--- app/auto-typer/auto-typer.component.ts
+++ app/auto-typer/auto-typer.component.ts
@@ -10,14 +10,21 @@ typedStrings: Array<string> = [];
 loopNum = 0;
 txt = '';
 isDeleting = false;
+toRotate: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  textType(el, loop, period) {
-
+  tick(el, loop, period) {
+    let i = this.loopNum % this.toRotate.length;
+    let fullText = this.toRotate[i];
+    if (this.isDeleting) {
+      this.txt = fullText.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullText.substring(0, this.txt.length + 1);
+    }
   }
 
 }
